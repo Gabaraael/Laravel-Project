b8a34b65b3ea28edeb7aac9322bf1255877ad86e
Rooster Figther <gabriel1998.gp45@gmail.com>
2023-03-23 22:25:52 -0400

    Integrando com o banco postgres

--- app/Http/Controllers/DinossauroController.php
+++ app/Http/Controllers/DinossauroController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DinossauroController extends Controller
 {
     public function listarDino() {
-       $dino['name'] = 'T-rex';
+       $dinos = DB::select('SELECT * FROM dinosaur ORDER BY NAME');
+        $dino['name'] = 'T-rex';
        $dino['idade'] = '17';
        $dino['velocidade'] = '42km/h';
        $dino['altura'] = '4M';
        $dino = json_encode($dino);
-        return $dino;
+        return $dinos;
     }
 }
 
--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,10 +67,13 @@ return [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // Mudar pra rodar com .env
+            'port' => env('DB_PORT', '5433'),
+            // Mudar pra rodar com .env
+            'database' => env('DB_DATABASE', 'laravel'),
+            // Mudar pra rodar com .env
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
