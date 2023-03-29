324170032f96873ef21dab873de55bf52d413056
aluno <gabriel1998.gp45@gmail.com>
2023-03-28 22:13:53 -0400

    Corrigindo acesso ao banco

--- app/Http/Controllers/DinossauroController.php
+++ app/Http/Controllers/DinossauroController.php
@@ -9,12 +9,14 @@ class DinossauroController extends Controller
 {
     public function listarDino() {
        $dinos = DB::select('SELECT * FROM dinosaur ORDER BY NAME');
-        $dino['name'] = 'T-rex';
+
+       $dino['name'] = 'T-rex';
        $dino['idade'] = '17';
        $dino['velocidade'] = '42km/h';
        $dino['altura'] = '4M';
        $dino = json_encode($dino);
-        return $dinos;
+
+       return $dinos;
     }
 }
 
